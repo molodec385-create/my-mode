3f0bdf3dcb09e3a01533cdcfb68990089282b012
molodec385-create <molodec385@gmail.com>
2026-06-14 11:11:50 +0300

    Delete team_scores.js

--- team_scores.js
+++
@@ -1,8 +0,0 @@
-export const TEAM_SCORES_MUL = 0.08;
-export const SCORES_PROP_NAME = "Scores";
-
-export function addTeamScores(team, score) {
-    if (!team) return;
-    let scaled = Math.round(score * TEAM_SCORES_MUL);
-    team.Properties.Scores.Value += scaled;
-}
