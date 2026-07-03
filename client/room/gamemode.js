d317a0379a353970fc8d7e38d04cf0178fd8f4c8
molodec385-create <molodec385@gmail.com>
2026-07-03 12:43:20 +0300

    Update zombie and inmate team colors

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -21,8 +21,8 @@ const ZOMBIE_TEAM_DISPLAY_NAME = "Teams/Zombies";
 const INMATE_TEAM_DISPLAY_NAME = "Teams/Inmates";
 const ZOMBIE_SPAWN_POINTS_GROUP = 1;
 const INMATE_SPAWN_POINTS_GROUP = 2;
-const ZOMBIE_TEAM_COLOR = { b: 3 };
-const INMATE_TEAM_COLOR = { r: 4 };
+const ZOMBIE_TEAM_COLOR = { b: 1 };
+const INMATE_TEAM_COLOR = { r: 2 };
 
 function create_team_zombies() {
 	Teams.Add(ZOMBIE_TEAM_NAME, ZOMBIE_TEAM_DISPLAY_NAME, ZOMBIE_TEAM_COLOR);
